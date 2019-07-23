e03fb2be2a9c79d366abe61a395f4438fce2cbad
patriba <36122767+patriba@users.noreply.github.com>
2019-07-23 12:31:21 +1000

    end of Part 9 - Containers

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Intro from '../Intro';
 import 'whatwg-fetch';
+import Series from '../../containers/Series';
 
 //both are correct - same result
 //function App() {
@@ -10,17 +11,6 @@ import 'whatwg-fetch';
 //  render() {
 
 class App extends Component {
-  state = {
-    series: []
-  }
-
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-    .then((response) => response.json())
-    .then(json => this.setState({
-      series: json
-    }));
-  }
   render() {
     return (
       <div className="App">
@@ -28,7 +18,8 @@ class App extends Component {
           <h1 className="App-title">TV Series List</h1>
         </header>
         <Intro message="Here you can find all of your most loved series"/>
-        The length of series array - {this.state.series.length}
+        
+        <Series />
       </div>
     );
   }
