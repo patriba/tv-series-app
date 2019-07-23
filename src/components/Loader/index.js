9ec6c7414440c1a29d6d8631d70ab28a43aeddb8
patriba <36122767+patriba@users.noreply.github.com>
2019-07-23 13:29:34 +1000

    end of Part 14 - Images and initial TSX change

---
+++ src/components/Loader/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import loaderSrc from '../../assets/loader.gif';
+
+const Loader = props => (
+    <div>
+        <img style={{width: 75}} src={loaderSrc} alt="Loader icon" />
+    </div>
+);
+
+export default Loader;
